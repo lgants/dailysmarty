ac5dfbcb04859914a9abbb3d61e5b5fdf18b8328
lgants <lgants@gmail.com>
2017-01-27 16:50:29 -0600

    implemented friendly id gem

---
+++ spec/models/topic_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Topic, type: :model do
+  before do
+    @topic = Topic.create(title: "Sports")
+  end
+
+  describe 'validations' do
+    it 'cannot be created without a title' do
+      @topic.title = nil
+      expect(@topic).to_not be_valid
+    end
+
+    it 'cannot be created without a slug' do
+      @topic.slug = nil
+      expect(@topic).to_not be_valid
+    end
+  end
+end
