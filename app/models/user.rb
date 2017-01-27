98e9af706014b01c88af760a9fe988b5cfb86349
lgants <lgants@gmail.com>
2017-01-27 14:43:51 -0600

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_initialize :set_defaults
+
+  validates_presence_of :first_name, :last_name, :username
+
+  # to prevent complications when usernames used as subdomains
+  validates :username, uniqueness: true, presence: true, format: { with: /\A[a-zA-Z]+([a-zA-Z]|\d)*\Z/ }
+
+
+
+  private
+
+    def set_defaults
+      self.role ||= 'student'
+    end
 end
