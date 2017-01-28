afd1733d394532c30aeee0e31fd64e190736304a
lgants <lgants@gmail.com>
2017-01-27 21:16:03 -0600

    generated resources for Post

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,8 @@
 class Topic < ActiveRecord::Base
   extend FriendlyId
+
+  has_many :posts
+  
   friendly_id :title, use: :slugged
   validates_uniqueness_of :slug
   validates_presence_of :title, :slug
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :topics
   devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'static#home'
