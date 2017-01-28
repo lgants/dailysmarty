9f4bae93993a2cb02a8be2202ec8374c37ca1978
lgants <lgants@gmail.com>
2017-01-27 22:58:01 -0600

    updated views to render posts under a topic

--- app/controllers/topics/posts_controller.rb
+++ app/controllers/topics/posts_controller.rb
@@ -1,5 +1,14 @@
 # class PostsController < ApplicationController
 class Topics::PostsController < ApplicationController
+  before_action :set_topic
+
   def index
+    @posts = @topic.posts
   end
+
+  private
+
+    def set_topic
+      @topic = Topic.friendly.find(params[:topic_id])
+    end
 end
